dbf06437ce02d42e91ff3755c8a30590eca372b0
Aldhair <u201315853@upc.edu.pe>
2018-08-30 16:42:07 -0500

    Line Rider v 1.0

---
+++ Assets/LineCreator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour {
+
+    public List<GameObject> linePrefabs;
+    public GameObject current;
+    Line activeLine;
+
+    private void Start()
+    {
+        current = linePrefabs[0];
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            GameObject lineGO= Instantiate(current);
+            activeLine = lineGO.GetComponent<Line>();
+            activeLine.gameObject.AddComponent<LineDestroyer>();
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            //Remember that Input.mousePosition is in screen coordinates
+            //To work with them we must convert them to world coordinates
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+	}
+
+    public void CurrentLine(int line)
+    {
+        switch(line)
+        {
+            case 0:
+                current = linePrefabs[0];
+            break;
+            case 1:
+                current = linePrefabs[1];
+            break;
+            case 2:
+                current = linePrefabs[2];
+            break;
+            case 3:
+                current = linePrefabs[3];
+            break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+    public LineRenderer line;
+    public EdgeCollider2D edgeCol;
+
+    List<Vector2> points;
+
+    public float minDistance;
+
+    public void UpdateLine(Vector2 mousePosition)
+    {
+        //If the list is empty, then create a new point in the list
+        //With the position of the mousePosition
+        if(points==null)
+        {
+            points = new List<Vector2>();
+            SetPoint(mousePosition);
+            return;
+        }
+        //Check if the mouse has moved enough for us to insert new point
+        //If it has: Insert point at mouse position
+        //points[points.Count-1]= points.Last()
+        if (Vector2.Distance(points.Last(),mousePosition) > minDistance)
+        {
+            SetPoint(mousePosition);
+        }
+    }
+
+    void SetPoint(Vector2 point)
+    {
+        points.Add(point);
+        //Setting the number of positions in the line
+        line.positionCount = points.Count;
+        //Setting new index and points to the line
+        line.SetPosition(points.Count - 1,point);
+        //Setting the points of edge collider
+        if(points.Count>1)
+            edgeCol.points = points.ToArray();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
